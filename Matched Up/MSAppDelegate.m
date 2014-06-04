6036256ae30c6a14bb87127d0f96534a2cc2bc1d
Mat Sletten <matsletten@pascal-pelous-macbook-pro.bbbb.net>
2014-06-03 17:24:56 -0700

    method fix

--- Matched Up/MSAppDelegate.m
+++ Matched Up/MSAppDelegate.m
@@ -39,12 +39,12 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
