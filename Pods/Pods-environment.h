9cd4d10b4cad50c31f1d3876120e3830b59360c0
Mat Sletten <matsletten@MSLETTENMBP.local>
2014-06-20 09:57:24 -0700

    added JSMessagesVC

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -15,8 +15,20 @@
 // Facebook-iOS-SDK
 #define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
 #define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
-#define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 14
-#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 1
+#define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 15
+#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 0
+
+// JSMessagesViewController
+#define COCOAPODS_POD_AVAILABLE_JSMessagesViewController
+#define COCOAPODS_VERSION_MAJOR_JSMessagesViewController 3
+#define COCOAPODS_VERSION_MINOR_JSMessagesViewController 4
+#define COCOAPODS_VERSION_PATCH_JSMessagesViewController 0
+
+// JSQSystemSoundPlayer
+#define COCOAPODS_POD_AVAILABLE_JSQSystemSoundPlayer
+#define COCOAPODS_VERSION_MAJOR_JSQSystemSoundPlayer 1
+#define COCOAPODS_VERSION_MINOR_JSQSystemSoundPlayer 5
+#define COCOAPODS_VERSION_PATCH_JSQSystemSoundPlayer 2
 
 // Parse
 #define COCOAPODS_POD_AVAILABLE_Parse
